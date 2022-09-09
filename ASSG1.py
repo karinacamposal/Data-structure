0e4c59cd3ffc974f267bb754465b176fd333b6ce
karinacamposal <2109020@upy.edu.mx>
2022-09-08 19:43:01 -0500

    Assigment 1

--- ASSG1.py
+++ ASSG1.py
@@ -2,12 +2,12 @@ print("Bienvenido a la Universidad ABC")
 
 print("Introduzca la informción necesaria")
 
-Nombre = str(input("Escribe tu nombre: "))
+Nombre = input("Escribe tu nombre: ")
 
 Telefono = int(input("Escribe tu telefono: "))
 
-correo = str(input("Escribe tu correo: "))
+correo = input("Escribe tu correo: ")
 
-curso = str(input("Elija su curso: "))
+curso = input("Elija su curso: ")
 
-print(f'{Nombre},has elegido el {curso} como tu curso, con telefono {Telefono}')
+print(f'{Nombre},has elegido el {curso} como tu curso')
