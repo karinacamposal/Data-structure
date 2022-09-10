3fd598e0ddea6e6cb0fbf7d3087cdd9903a039a3
karinacamposal <2109020@upy.edu.mx>
2022-09-09 20:32:00 -0500

    Assigment 2

---
+++ ASSG2.py
@@ -0,0 +1,13 @@
+"""A university student decided to set up a program that will allow potential online students from 
+abroad to fill out the form and search for more information about the courses they are interested in."""""
+
+print("Welcome to ABC University \n")
+
+print("Please enter the necessary information")
+
+name = input("Enter your full name: ")
+pnumber = int(input("Enter your phone number: "))
+email = input("Enter your email: ")
+course = input("Select the course you want to take: ")
+
+print(f'\n{name}, you have chosen the {course} as your course.')
